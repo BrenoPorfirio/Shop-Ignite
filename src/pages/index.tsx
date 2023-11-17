0dc37413e69e82b94b1611630721eb23494ced21
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-16 22:02:58 -0300

    testing styles from Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,24 @@ import { styled } from '../styles'
 
 const Button = styled('button', {
   backgroundColor: '$rocketseat',
+  borderRadius: 8,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
 })
 
 export default function Home() {
-  return <Button>ENVIAR</Button>
+  return (
+    <Button>
+      <span>Teste</span>
+      ENVIAR
+    </Button>
+  )
 }
