ae99fac34788733ae1ea7b5731759bf3b57b678f
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-18 20:20:02 -0300

    import logo in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header>
+        <img src={logoImg.src} />
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
