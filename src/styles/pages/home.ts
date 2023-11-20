3d46ca34e7b4fd1f98e8ab83b003ca4ae3b5cd84
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-20 00:15:46 -0300

    fix linear-gradient

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -10,7 +10,7 @@ export const HomeContainer = styled('main', {
 })
 
 export const Product = styled('a', {
-  background: 'lineargradient(180deg, #1ea483 0%, #7465d4 100%)',
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
   borderRadius: 8,
   padding: '0.25rem',
   cursor: 'pointer',
@@ -39,6 +39,12 @@ export const Product = styled('a', {
 
     strong: {
       fontsize: '$lg',
+    },
+
+    span: {
+      fontSize: '$xl',
+      fontWeight: 'bold',
+      color: '$green300',
     },
   },
 })
