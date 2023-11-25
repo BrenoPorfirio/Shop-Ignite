a2ae1f3f6ff0ca81df9a97ae26f589cc0bcc01c8
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-11-24 22:55:17 -0300

    change name to getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import { HomeContainer, Product } from '../styles/pages/home'
-import camiseta1 from '../assets/camisetas/1.png'
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Stripe from 'stripe'
 
 interface Homeprops {
@@ -40,7 +39,7 @@ export default function Home({ products }: Homeprops) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   })
