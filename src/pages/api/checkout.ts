b08c78f7fb73bcb38a92e5b52ac29ca9fcd67060
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-15 18:35:41 -0300

    add error to status 405 and 400

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,7 +5,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  const priceID = 'price_1OFQl0Li7n4COKcFRhF3PEK5'
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found' })
+  }
 
   const successUrl = `${process.env.NEXT_URL}/sucess`
   const cancelUrl = `${process.env.NEXT_URL}/`
@@ -16,7 +24,7 @@ export default async function handler(
     mode: 'payment',
     line_items: [
       {
-        price: priceID,
+        price: priceId,
         quantity: 1,
       },
     ],
