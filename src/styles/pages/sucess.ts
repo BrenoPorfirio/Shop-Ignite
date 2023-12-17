d81f9c65fff5f5ce3b6f645e586f09d51737057c
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-16 22:16:50 -0300

    style to ImageContainer

--- src/styles/pages/sucess.ts
+++ src/styles/pages/sucess.ts
@@ -9,4 +9,19 @@ export const SucessContainer = styled('main', {
   height: 656,
 })
 
-export const ImageContainer = styled('div', {})
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 130,
+  height: 145,
+  background: 'linear-gradient(188deg, #1ea483, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+})
