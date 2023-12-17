4f570e9eec39a2fc6ba88035f71c990761b7d56f
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-16 22:23:23 -0300

    style sucess

--- src/styles/pages/success.ts
+++ src/styles/pages/success.ts
@@ -7,6 +7,33 @@ export const SucessContainer = styled('main', {
   justifyContent: 'center',
   margin: '0 auto',
   height: 656,
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray100',
+  },
+
+  p: {
+    fontSize: '$xl',
+    color: '$gray300',
+    maxWidth: 560,
+    textAlign: 'center',
+    marginTop: '2rem',
+    lineHeight: 1.4,
+  },
+
+  a: {
+    display: 'block',
+    marginTop: '5rem',
+    fontSize: '$lg',
+    color: '$green500',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+
+    '&:hover': {
+      color: '$green300',
+    },
+  },
 })
 
 export const ImageContainer = styled('div', {
@@ -16,6 +43,7 @@ export const ImageContainer = styled('div', {
   background: 'linear-gradient(188deg, #1ea483, #7465d4 100%)',
   borderRadius: 8,
   padding: '0.25rem',
+  marginTop: '4rem',
 
   display: 'flex',
   alignItems: 'center',
