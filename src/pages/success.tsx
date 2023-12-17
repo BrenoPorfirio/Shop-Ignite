0d33d1248e3ed2b71dff3491edb2360fc135aeb3
Breno Costa Porfírio <brenoporfirio03@gmail.com>
2023-12-16 22:10:31 -0300

    structure to sucess

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from 'next/link'
+import { ImageContainer, SucessContainer } from '../styles/pages/sucess'
+
 export default function Success() {
-  return <h1>Success</h1>
+  return (
+    <SucessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer>
+        <p>
+          Uhuul <strong>Breno Costa</strong>, sua
+          <strong>Camiseta Beyond The Limits</strong> já está a caminho do seu
+          destino.
+        </p>
+
+        <Link href="/">Voltar ao catálogo</Link>
+      </ImageContainer>
+    </SucessContainer>
+  )
 }
